8ef76fd2b0a9320812b848de4b6b64145f5b9157
nawsish <nawsish2018@gmail.com>
2025-06-26 14:47:48 +0600

    add dto

--- ticket_System_API/ticket_System_API/Data/ApplicationDbContext.cs
+++ ticket_System_API/ticket_System_API/Data/ApplicationDbContext.cs
@@ -1,6 +1,16 @@
-﻿namespace ticket_System_API.Data
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using ticket_System_API.Models;
+
+namespace ticket_System_API.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<ticket> Tickets { get; set; }
+        public DbSet<ticketResponse> TicketResponses { get; set; }
     }
 }
